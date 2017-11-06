a5a8de29f7116d1c8f722bf80db05d7e0f68195e
Simon Roberts <simon@dancingcloudservices.com>
2017-11-06 12:13:34 -0700

    Illustrate Comparator<Car> and sort order

--- src/main/java/functional/CarScratch.java
+++ src/main/java/functional/CarScratch.java
@@ -2,7 +2,15 @@ package functional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+
+class PassengerCountOrder implements Comparator<Car> {
+  @Override
+  public int compare(Car o1, Car o2) {
+    return o1.getPassengers().size() - o2.getPassengers().size();
+  }
+}
 
 public class CarScratch {
   public static void showAll(List<Car> lc) {
@@ -34,14 +42,17 @@ public class CarScratch {
   
   public static void main(String[] args) {
     List<Car> cars = Arrays.asList(
+        Car.withGasColorPassengers(7, "Green", "Valentine", "Gillian", "Anne", "Dr. Mahmoud"),
         Car.withGasColorPassengers(6, "Red", "Fred", "Jim", "Sheila"),
         Car.withGasColorPassengers(3, "Octarine", "Rincewind", "Ridcully"),
-        Car.withGasColorPassengers(9, "Black", "Weatherwax", "Magrat"),
-        Car.withGasColorPassengers(7, "Green", "Valentine", "Gillian", "Anne", "Dr. Mahmoud"),
-        Car.withGasColorPassengers(6, "Red", "Ender", "Hyrum", "Locke", "Bonzo")
+        Car.withGasColorPassengers(6, "Red", "Ender", "Hyrum", "Locke", "Bonzo"),
+        Car.withGasColorPassengers(9, "Black", "Weatherwax", "Magrat")
     );
     showAll(cars);
     
     showAll(getCarsByColor(cars, "Red"));
+    
+    cars.sort(new PassengerCountOrder());
+    showAll(cars);
   }
 }
