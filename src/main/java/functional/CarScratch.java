f9b3729e715646d65d8a41b3194a72f01c178f54
Simon Roberts <simon@dancingcloudservices.com>
2017-11-06 12:19:55 -0700

    Clean out sort ordering Create CarCriterion interface Implement as RedCarCriterion Replace two getCarsBy... with single getCarsByCriterion Invoke filtering using new system

--- src/main/java/functional/CarScratch.java
+++ src/main/java/functional/CarScratch.java
@@ -2,13 +2,16 @@ package functional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
-class PassengerCountOrder implements Comparator<Car> {
+interface CarCriterion {
+  boolean test(Car c);
+}
+
+class RedCarCriterion implements CarCriterion {
   @Override
-  public int compare(Car o1, Car o2) {
-    return o1.getPassengers().size() - o2.getPassengers().size();
+  public boolean test(Car c) {
+    return c.getColor().equals("Red");
   }
 }
 
@@ -20,39 +23,28 @@ public class CarScratch {
     System.out.println("-------------------------------------");
   }
   
-  public static List<Car> getCarsByColor(Iterable<Car> lc, String color) {
+  
+  public static List<Car> getCarsByCriterion(Iterable<Car> lc, CarCriterion criterion) {
     List<Car> rv = new ArrayList<>();
     for (Car c : lc) {
-      if (c.getColor().equals(color)) {
+      if (criterion.test(c)) {
         rv.add(c);
       }
     }
     return rv;
   }
-  
-  public static List<Car> getCarsByMinGasLevel(Iterable<Car> lc, int minGas) {
-    List<Car> rv = new ArrayList<>();
-    for (Car c : lc) {
-      if (c.getGasLevel() >= minGas) {
-        rv.add(c);
-      }
-    }
-    return rv;
-  }
-  
+
   public static void main(String[] args) {
     List<Car> cars = Arrays.asList(
-        Car.withGasColorPassengers(7, "Green", "Valentine", "Gillian", "Anne", "Dr. Mahmoud"),
         Car.withGasColorPassengers(6, "Red", "Fred", "Jim", "Sheila"),
         Car.withGasColorPassengers(3, "Octarine", "Rincewind", "Ridcully"),
-        Car.withGasColorPassengers(6, "Red", "Ender", "Hyrum", "Locke", "Bonzo"),
-        Car.withGasColorPassengers(9, "Black", "Weatherwax", "Magrat")
+        Car.withGasColorPassengers(9, "Black", "Weatherwax", "Magrat"),
+        Car.withGasColorPassengers(7, "Green", "Valentine", "Gillian", "Anne", "Dr. Mahmoud"),
+        Car.withGasColorPassengers(6, "Red", "Ender", "Hyrum", "Locke", "Bonzo")
     );
     showAll(cars);
     
-    showAll(getCarsByColor(cars, "Red"));
+    showAll(getCarsByCriterion(cars, new RedCarCriterion()));
     
-    cars.sort(new PassengerCountOrder());
-    showAll(cars);
   }
 }
