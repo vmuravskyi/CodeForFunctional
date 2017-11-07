8f95ea5234569acbdc1efcd30b41fd7c9b7c7135
Simon Roberts <simon@dancingcloudservices.com>
2017-11-07 12:53:35 -0700

    Null checks for getting trunk contents

--- src/main/java/functional/NullChecks.java
+++ src/main/java/functional/NullChecks.java
@@ -10,10 +10,16 @@ public class NullChecks {
     owners.put("Sheila", Car.withGasColorPassengers(6, "Red", "Fred", "Jim", "Sheila"));
     owners.put("Librarian", Car.withGasColorPassengers(3, "Octarine", "Rincewind", "Ridcully"));
     owners.put("Ogg", Car.withGasColorPassengersAndTrunk(9, "Black", "Weatherwax", "Magrat"));
-    
+
     String owner = "Ogg";
     Car c = owners.get(owner);
-    List<String> trunkItems = c.getTrunkContents();
-    System.out.println(owner + " has " + trunkItems + " in the car");
+    if (c != null) {
+      List<String> trunkItems = c.getTrunkContents();
+      if (trunkItems != null) {
+        System.out.println(owner + " has " + trunkItems + " in the car");
+      } else {
+        System.out.println(owner + " has nothing in the car");
+      }
+    }
   }
 }
