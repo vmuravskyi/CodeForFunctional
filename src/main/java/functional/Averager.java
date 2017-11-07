d537e0a35775947d52d1bcea900b5f9e3aa4a50c
Simon Roberts <simon@dancingcloudservices.com>
2017-11-07 15:08:29 -0700

    Parallel collect version with math.sin calculation

--- src/main/java/functional/Averager.java
+++ src/main/java/functional/Averager.java
@@ -1,5 +1,6 @@
 package functional;
 
+import java.util.Spliterator;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.DoubleStream;
 
@@ -24,10 +25,21 @@ class Average {
 
 public class Averager {
   public static void main(String[] args) {
+    long start = System.nanoTime();
     Average av = DoubleStream.generate(() -> ThreadLocalRandom.current().nextDouble(-Math.PI, Math.PI))
-        .limit(1_000)
-        .collect(() -> new Average(), (r, d) -> r.include(d), (r1, r2) -> r1.merge(r2));
+        .parallel()
+//        .unordered()
+//        .limit(4_000_000_000L)
+        .limit(200_000_000)
+        .map(x -> Math.sin(x))
+        .collect(
+            () -> new Average(), 
+            (r, d) -> r.include(d), 
+            (r1, r2) -> r1.merge(r2));
+    long end = System.nanoTime();
+    
     double mean = av.get();
     System.out.println("Average is " + mean);
+    System.out.println("Time taken: " + ((end - start) / 1_000_000) + " ms");
   }
 }
