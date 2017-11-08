c747c7d3845a1f6c459de91893c848f06f659240
Simon Roberts <simon@dancingcloudservices.com>
2017-11-08 11:06:55 -0700

    Non working attempt at concordance on multiple files

--- src/main/java/functional/Concordance.java
+++ src/main/java/functional/Concordance.java
@@ -2,8 +2,11 @@ package functional;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.regex.Pattern;
@@ -11,22 +14,25 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Concordance {
+
   private static final Pattern WORD_BREAK = Pattern.compile("\\W+");
-  private static final Comparator<Map.Entry<String, Long>> valueOrder =
-      Map.Entry.comparingByValue();
-  
+  private static final Comparator<Map.Entry<String, Long>> valueOrder
+      = Map.Entry.comparingByValue();
+
   public static void main(String[] args) throws IOException {
-    try (Stream<String> lines = Files.lines(Paths.get("PrideAndPrejudice.txt"))) {
-      lines
-          .map(String::toLowerCase)
-          .flatMap(WORD_BREAK::splitAsStream)
-          .filter(s -> s.length() > 0)
-          .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-          .entrySet().stream()
-          .sorted(valueOrder.reversed())
-          .limit(200)
-          .map(e -> String.format("%20s : %5d", e.getKey(), e.getValue()))
-          .forEach(System.out::println);
-    }
+    List<String> filenames = Arrays.asList("PrideAndPrejudice.txt", "Bad.txt", "Emma.txt", "SenseAndSensibility.txt");
+    filenames.stream()
+        .map(Paths::get)
+        .flatMap(Files::lines)
+        .map(String::toLowerCase)
+        .flatMap(WORD_BREAK::splitAsStream)
+        .filter(s -> s.length() > 0)
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+        .entrySet().stream()
+        .sorted(valueOrder.reversed())
+        .limit(200)
+        .map(e -> String.format("%20s : %5d", e.getKey(), e.getValue()))
+        .forEach(System.out::println);
   }
 }
+
