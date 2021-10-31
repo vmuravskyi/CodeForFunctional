14394a949061a2c7db65add1a29a640c3321e344
vmuravskyi <v.muravskyi@gmail.com>
2021-10-31 17:09:07 +0200

    initial commit

--- src/main/java/functional/Car.java
+++ src/main/java/functional/Car.java
@@ -1,93 +1,83 @@
 package functional;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class Car {
 
-  private final int gasLevel;
-  private final String color;
-  private final List<String> passengers;
-  private final List<String> trunkContents;
+    private static final Predicate<Car> RED_CAR_CRITERION
+            = c -> c.color.equals("Red");
+    private static final Comparator<Car> fuelComparator = (o1, o2) -> o1.gasLevel - o2.gasLevel;
+    private final int gasLevel;
+    private final String color;
+    private final List<String> passengers;
+    private final List<String> trunkContents;
 
-  private Car(int gasLevel, String color, List<String> passengers, List<String> trunkContents) {
-    this.gasLevel = gasLevel;
-    this.color = color;
-    this.passengers = passengers;
-    this.trunkContents = trunkContents;
-  }
+    private Car(int gasLevel, String color, List<String> passengers, List<String> trunkContents) {
+        this.gasLevel = gasLevel;
+        this.color = color;
+        this.passengers = passengers;
+        this.trunkContents = trunkContents;
+    }
 
-  public static Car withGasColorPassengers(int gas, String color, String... passengers) {
-    List<String> p = Collections.unmodifiableList(Arrays.asList(passengers));
-    Car self = new Car(gas, color, p, null);
-    return self;
-  }
+    public static Car withGasColorPassengers(int gas, String color, String... passengers) {
+        List<String> p = Collections.unmodifiableList(Arrays.asList(passengers));
+        return new Car(gas, color, p, null);
+    }
 
-  public static Car withGasColorPassengersAndTrunk(int gas, String color, String... passengers) {
-    List<String> p = Collections.unmodifiableList(Arrays.asList(passengers));
-    Car self = new Car(gas, color, p, Arrays.asList("jack", "wrench", "spare wheel"));
-    return self;
-  }
+    public static Car withGasColorPassengersAndTrunk(int gas, String color, String... passengers) {
+        List<String> p = Collections.unmodifiableList(Arrays.asList(passengers));
+        return new Car(gas, color, p, Arrays.asList("jack", "wrench", "spare wheel"));
+    }
 
-  public int getGasLevel() {
-    return gasLevel;
-  }
-  
-  public Car addGas(int g) {
-    return new Car(gasLevel + g, color, passengers, trunkContents);
-  }
+    public static Predicate<Car> getColorCriterion(String... colors) {
+        Set<String> colorSet = new TreeSet<>(Arrays.asList(colors));
+        return c -> colorSet.contains(c.getColor());
+    }
 
-  public String getColor() {
-    return color;
-  }
+    public static Predicate<Car> getRedCarCriterion() {
+        return RED_CAR_CRITERION;
+    }
 
-  public List<String> getPassengers() {
-    return passengers;
-  }
+    public static Predicate<Car> getGasLevelCarCriterion(final int threshold) {
+        return c -> c.gasLevel >= threshold;
+    }
 
-  public List<String> getTrunkContents() {
-    return trunkContents;
-  }
+    public static Predicate<Car> getFourPassengerCriterion() {
+        return c -> c.passengers.size() >= 4;
+    }
 
-  public Optional<List<String>> getTrunkContentsOpt() {
-    return Optional.ofNullable(trunkContents);
-  }
+    public static Comparator<Car> getFuelComparator() {
+        return fuelComparator;
+    }
 
-  @Override
-  public String toString() {
-    return "Car{" + "gasLevel=" + gasLevel + ", color=" + color + ", passengers=" + passengers
-        + (trunkContents != null ? ", trunkContents=" + trunkContents : " no trunk") + '}';
-  }
+    public int getGasLevel() {
+        return gasLevel;
+    }
 
-  public static Predicate<Car> getColorCriterion(String ... colors) {
-    Set<String> colorSet = new TreeSet<>(Arrays.asList(colors));
-    return c -> colorSet.contains(c.getColor());
-  }
-  
-  public static Predicate<Car> getRedCarCriterion() {
-    return RED_CAR_CRITERION;
-  }
+    public Car addGas(int g) {
+        return new Car(gasLevel + g, color, passengers, trunkContents);
+    }
 
-  private static final Predicate<Car> RED_CAR_CRITERION
-      = c -> c.color.equals("Red");
+    public String getColor() {
+        return color;
+    }
 
-  public static Predicate<Car> getGasLevelCarCriterion(final int threshold) {
-    return c -> c.gasLevel >= threshold;
-  }
+    public List<String> getPassengers() {
+        return passengers;
+    }
 
-  public static Predicate<Car> getFourPassengerCriterion() {
-    return c -> c.passengers.size() >= 4;
-  }
+    public List<String> getTrunkContents() {
+        return trunkContents;
+    }
 
-  public static Comparator<Car> getFuelComparator() {
-    return fuelComparator;
-  }
+    public Optional<List<String>> getTrunkContentsOpt() {
+        return Optional.ofNullable(trunkContents);
+    }
 
-  private static final Comparator<Car> fuelComparator = (o1, o2) -> o1.gasLevel - o2.gasLevel;
+    @Override
+    public String toString() {
+        return "Car{" + "gasLevel=" + gasLevel + ", color=" + color + ", passengers=" + passengers
+                + (trunkContents != null ? ", trunkContents=" + trunkContents : " no trunk") + '}';
+    }
 }
